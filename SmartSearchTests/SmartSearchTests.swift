04cd3655cf9e1958be6654e4fb26d26945cbff4e
Gleb S <glebright@gmail.com>
2018-09-19 20:59:26 +0300

    Initial Commit

---
+++ SmartSearchTests/SmartSearchTests.swift
@@ -0,0 +1,34 @@
+//
+//  SmartSearchTests.swift
+//  SmartSearchTests
+//
+//  Created by Gleb Shendrik on 19/09/2018.
+//  Copyright © 2018 Gleb Shendrik. All rights reserved.
+//
+
+import XCTest
+@testable import SmartSearch
+
+class SmartSearchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
