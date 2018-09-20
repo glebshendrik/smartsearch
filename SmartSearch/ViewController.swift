707e855373080c72f08832c555483bdab77cc794
Gleb S <glebright@gmail.com>
2018-09-20 10:03:15 +0300

    CoreML

--- SmartSearch/ViewController.swift
+++ SmartSearch/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import CoreML
+import NaturalLanguage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputName: UITextField!
+    @IBOutlet weak var textName: UILabel!
+    var searchName = [String]()
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    //    MARK: Predict, Model
+    func validateName (str: String) {
+        let url = Bundle.main.url(forResource: "names", withExtension: "mlmodelc")!
+        guard let model = try? NLModel(contentsOf: url) else {
+            return
+        }
+        let conclusion = model.predictedLabel(for: str)
+        print(conclusion as Any)
+        textName.text = conclusion
+    }
+    
+    @IBAction func predictName(_ sender: Any) {
+        validateName(str: inputName.text!)
+    }
+    
 }
 
